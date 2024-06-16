21606f92aa8e7c555bef78c5cd11e2105561ce95
LBD <lyanbasora@gmail.com>
2024-06-16 13:32:56 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// .reduce((args) => {do something}, second param), .filter((args) => {})
+// result = function; result <-- then value of variable appears/updates?
+// ternary operator (condition) ? {value if true}:{value if false}
+
+// ---- if (function({operation}) !== value) {throw new Error('Error message')} ---
+
+
